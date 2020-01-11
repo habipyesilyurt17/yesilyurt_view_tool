ae8f63aa16052f4ff3bf3043227aa3d1202c90be
Habip Yesilyurt <habip.yesilyurt@nimbo.com.tr>
2020-01-11 10:30:11 +0300

    YesilyurtViewtool initial commit

---
+++ lib/yesilyurt_view_tool.rb
@@ -0,0 +1,5 @@
+require "yesilyurt_view_tool/version"
+require "yesilyurt_view_tool/renderer"
+
+module YesilyurtViewTool
+end
---
+++ lib/yesilyurt_view_tool/renderer.rb
@@ -0,0 +1,7 @@
+module YesilyurtViewTool
+  class Renderer
+    def self.copyright(name,msg)
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end 
+end
